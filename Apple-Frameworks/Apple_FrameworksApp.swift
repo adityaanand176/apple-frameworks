32f802da0ccce2d2b9bc68bf4a612b2b45c6cdc2
adityaanand176 <anandaditya.ind@gmail.com>
2024-06-17 22:37:46 +0530

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Aditya Anand on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
